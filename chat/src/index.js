256920e7aea3b25010311d36bbbcb0960ba2de74
Aaron Nguyen <aaronluannguyen@gmail.com>
2018-02-19 15:15:31 -0800

    Initialized Firebase in index js file

--- chat/src/index.js
+++ chat/src/index.js
@@ -4,5 +4,19 @@ import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
+import firebase from "firebase/app";
+
+// Initialize Firebase
+var config = {
+    apiKey: "AIzaSyCxCPv0SaV9KrL0rlkgeMRY09aKJREhLtg",
+    authDomain: "chat-app-ed855.firebaseapp.com",
+    databaseURL: "https://chat-app-ed855.firebaseio.com",
+    projectId: "chat-app-ed855",
+    storageBucket: "",
+    messagingSenderId: "567791393896"
+};
+firebase.initializeApp(config);
+
+
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
