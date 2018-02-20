feacea2cc1013c3399afd73abe872cb3cb4467ff
Aaron Nguyen <aaronluannguyen@gmail.com>
2018-02-19 23:26:23 -0800

    Added bootstrap and start basic layout of sign in, sign up, and main view screens

--- chat/src/App.js
+++ chat/src/App.js
@@ -1,24 +1,27 @@
 import React, { Component } from 'react';
 import {HashRouter as Router, Switch, Redirect, Route} from "react-router-dom";
 import {ROUTES} from "./constants";
-import logo from './logo.svg';
 import './App.css';
 import firebase from "firebase/app";
 import "firebase/auth";
 import "firebase/database";
 
+// Import Views
+import SignInView from "./Views/SignIn";
+import SignUpView from "./Views/SignUp";
+import MainView from "./Views/MainView"
+
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+      <Router>
+          <Switch>
+              <Route exact path={ROUTES.signIn} component={SignInView} />
+              <Route path={ROUTES.signUp} component={SignUpView}/>
+              <Route path={ROUTES.main} component={MainView}/>
+              <Redirect to={ROUTES.signIn}/>
+          </Switch>
+      </Router>
     );
   }
 }
