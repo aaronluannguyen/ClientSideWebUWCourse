07e48788e2ca23017045cc5c8f4bdd3551fe2b9b
aaronluannguyen <aaronluannguyen@gmail.com>
2018-02-25 15:15:47 -0800

    Saving progress on message related components

--- chat/src/Components/Message.js
+++ chat/src/Components/Message.js
@@ -2,6 +2,12 @@ import React from "react";
 
 export default class Message extends React.Component {
     render() {
+        let message = this.props.messageSnap.val();
 
+        return (
+            <div>
+
+            </div>
+        );
     }
 }
