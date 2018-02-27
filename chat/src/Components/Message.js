e8efa3b3a1a0d152c6980805a345d22195cc00a5
aaronluannguyen <aaronluannguyen@gmail.com>
2018-02-27 00:32:16 -0800

    Added stylesheet for message component

--- chat/src/Components/Message.js
+++ chat/src/Components/Message.js
@@ -1,17 +1,25 @@
 import React from "react";
+import firebase from "firebase/app";
+import "firebase/auth";
+import "firebase/database";
+import "./Message.css";
 
 export default class Message extends React.Component {
+    getTimeStamp(stamp) {
+        firebase.ref
+    }
+
     render() {
         let message = this.props.messageSnap.val();
 
         return (
             <div className="container">
-                <div className="row">
+                <div className="imgContainer">
                     <div className="col-1">
                         <img src={message.author.photoURL} alt="profile picture"/>
                     </div>
-                    <div className="col-11">
-                        <p>{message.author.displayName} ------ Posted at: {message.createdAt}</p>
+                    <div className="wordContent">
+                        <p>{message.author.displayName} ------ <span id="timestamp">Posted at: {message.createdAt}</span></p>
                         <p>{message.body}</p>
                     </div>
                 </div>
